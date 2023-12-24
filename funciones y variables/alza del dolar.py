e8fa96d4e1036fcfc82b0b2f6e284b6b154c2215
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2023-12-24 08:32:14 -0500

    new file:   "funciones y variables/# Una m\303\241quina de alimentos tiene product.py" 	modified:   funciones y variables/alza del dolar.py

--- funciones y variables/alza del dolar.py
+++ funciones y variables/alza del dolar.py
@@ -1,32 +1,16 @@
-# def maxalza(x,nd,alza):
-#     alza_maxima=0
-#     for i in range(nd):
-#         r=0
-#         doltmp= int(input("valor del dolar dia:"))
-        
-#         if doltmp >= r or doltmp<= r :
-#             r=doltmp
-#         alzatmp=x-r
-#         if alzatmp>alza_maxima:
-#             alza_maxima=alzatmp
-#     return alza_maxima
-# nd= int(input("ingrese el numero de dias: "))
-# print("la mayor alza fue: ", maxalza(nd))
-
 def maxalza(nd):
     alza_maxima = 0
     r = 0
     for i in range(nd):
-        
         doltmp = int(input("Valor del dólar del día: "))
 
-        if doltmp >= r or doltmp <= r:
-            r = doltmp
-
-        alzatmp = doltmp - r
+        alzatmp = doltmp - r  # Calculate the increase before updating the reference value
 
         if alzatmp > alza_maxima:
             alza_maxima = alzatmp
+
+        if doltmp > r:
+            r = doltmp  # Update the reference value if the current value is greater
 
     return alza_maxima
 
