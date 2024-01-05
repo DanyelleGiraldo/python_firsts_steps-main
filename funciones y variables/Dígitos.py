df154d602f487f827df7e1bf4428e88cb72bef67
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-01-05 07:26:39 -0500

    new file:   a.py 	new file:   as.py 	new file:   estudiantes.json 	modified:   "funciones y variables/D\303\255gitos.py" 	new file:   "funciones y variables/Registro de Ventas y Productos \303\232nicos.py" 	new file:   noentendi/estudiantes.json 	new file:   noentendi/estudiantes.py 	new file:   nuevoArchivo.txt 	new file:   programas x/Expresiones con listas.py 	new file:   programas x/def palocult(palabra)_.py 	new file:   programas x/ejemplo lista.py 	new file:   programas x/hola.py 	new file:   programas x/import random.py 	new file:   programas x/lista.py 	new file:   programas x/print(_hola mundo_).py 	new file:   programas x/quizz ahorcado Danyelle Giraldo.py 	new file:   taller/__pycache__/utils.cpython-310.pyc 	new file:   taller/main.py 	new file:   taller/utils.py 	new file:   ventas_semana_pasada.csv

--- funciones y variables/Dígitos.py
+++ funciones y variables/Dígitos.py
@@ -1,9 +1,2 @@
-def longitud(x):
-    y= len(x)
-    return y
-
-
-
 x=str(input("ingrese numero: "))
-resultado= longitud(x)
-print("su numero tiene:", resultado, "digitos")
+print(x, "tiene", len(x), "digitos")
